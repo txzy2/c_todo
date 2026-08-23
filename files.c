bf2e148cd4496a87256a7906cb6f2d732a88e77d
txzy2 <txzy2@yandex.ru>
2026-08-23 22:41:39 +0300

    fix files

--- files.c
+++ files.c
@@ -2,32 +2,9 @@
 #include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #define READ_LENGTH 100
-
-size_t read_from_file_with_array(const char *filename, char arr[][30])
-{
-	FILE *rf = fopen(filename, "r");
-
-	if (rf == NULL)
-	{
-		return 0;
-	}
-
-	size_t line = 0;
-
-	while (line < 100 && fgets(arr[line], 30, rf))
-	{
-		arr[line][strcspn(arr[line], "\r\n")] = '\0';
-		line++;
-	}
-
-	fclose(rf);
-
-	return line;
-}
 
 bool write_to_file(const char *filename, const char *text, const char *mode)
 {
@@ -41,4 +18,25 @@ bool write_to_file(const char *filename, const char *text, const char *mode)
 	fclose(f);
 
 	return true;
+}
+
+void read_from_file(const char *filename)
+{
+	FILE *rf = fopen(filename, "r");
+
+	if (rf == NULL)
+	{
+		return;
+	}
+
+	char file_str[READ_LENGTH];
+	printf("READ FROM FILE (<line-num> <line-data>): \n");
+	int line = 1;
+	while (fgets(file_str, READ_LENGTH, rf))
+	{
+		printf("%d %s", line, file_str);
+		line++;
+	}
+
+	fclose(rf);
 }
